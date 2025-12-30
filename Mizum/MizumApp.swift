dd9283093bf4d9f2e8343146e1e56214bc758fe8
KiyohitoNara <kiyohitonara1216@gmail.com>
2025-12-30 14:07:08 +0900

    Initial commit

---
+++ Mizum/MizumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MizumApp.swift
+//  Mizum
+//
+//  Created by Kiyohito Nara on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct MizumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MizumTests/MizumTests.swift
@@ -0,0 +1,17 @@
+//
+//  MizumTests.swift
+//  MizumTests
+//
+//  Created by Kiyohito Nara on 2025/12/30.
+//
+
+import Testing
+@testable import Mizum
+
+struct MizumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
