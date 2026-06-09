5145873dc41e000c12705cbae3a87845fc5c4881
KiyohitoNara <kiyohitonara1216@gmail.com>
2026-06-10 07:48:56 +0900

    Add tests for DrinkAmount

---
+++ Packages/Core/Tests/FeatureTests/DrinkAmountTest.swift
@@ -0,0 +1,31 @@
+import Testing
+
+@testable import Feature
+
+struct DrinkAmountTest {
+    @Test
+    func hasExpectedRawValues() {
+        #expect(DrinkAmount.small.rawValue == 100)
+        #expect(DrinkAmount.medium.rawValue == 250)
+        #expect(DrinkAmount.large.rawValue == 500)
+    }
+
+    @Test
+    func buildsIdentifierFromRawValue() {
+        #expect(DrinkAmount.small.identifier == "io.github.kiyohitonara.mizum.action.drink_100.0")
+        #expect(DrinkAmount.medium.identifier == "io.github.kiyohitonara.mizum.action.drink_250.0")
+        #expect(DrinkAmount.large.identifier == "io.github.kiyohitonara.mizum.action.drink_500.0")
+    }
+
+    @Test
+    func buildsTitleFromRawValue() {
+        #expect(DrinkAmount.small.title == "Drink 100ml")
+        #expect(DrinkAmount.medium.title == "Drink 250ml")
+        #expect(DrinkAmount.large.title == "Drink 500ml")
+    }
+
+    @Test
+    func listsAllCases() {
+        #expect(DrinkAmount.allCases == [.small, .medium, .large])
+    }
+}
